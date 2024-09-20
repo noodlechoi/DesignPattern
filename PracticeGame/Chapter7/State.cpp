f0c11959f91d20998d0badc1df60c2dbf1dd0eb3
noodlechoi <choime321@gmail.com>
2024-09-20 18:18:42 +0900

    상태 패턴

--- PracticeGame/Chapter7/State.cpp
+++ PracticeGame/Chapter7/State.cpp
@@ -1,11 +1,11 @@
 #include "State.h"
 
-void StatePattern::Heroine::handleInput(Input input)
+void ENUM::Heroine::handleInput(Input input)
 {
 	// 상태에 따라 분기
 	switch (state_)
 	{
-	case StatePattern::STANDING:
+	case ENUM::STANDING:
 		if (input == PRESS_B) {
 			state_ = JUMPING;
 		}
@@ -14,24 +14,24 @@ void StatePattern::Heroine::handleInput(Input input)
 			chargeTime_ = 0;
 		}
 		break;
-	case StatePattern::JUMPING:
+	case ENUM::JUMPING:
 		if (input == PRESS_DOWN) {
 			state_ = DIVING;
 		}
 		break;
-	case StatePattern::DUCKING:
+	case ENUM::DUCKING:
 		if (input == RELEASE_DOWN) {
 			state_ = STANDING;
 		}
 		break;
-	case StatePattern::DIVING:
+	case ENUM::DIVING:
 		break;
 	default:
 		break;
 	}
 }
 
-void StatePattern::Heroine::update()
+void ENUM::Heroine::update()
 {
 	if (state_ == DUCKING) {
 		chargeTime_++;
--- PracticeGame/Chapter7/State.h
+++ PracticeGame/Chapter7/State.h
@@ -1,5 +1,34 @@
 #pragma once
 
+namespace ENUM
+{
+	enum Input
+	{
+		PRESS_B,
+		PRESS_DOWN,
+		RELEASE_DOWN,
+	};
+
+	enum State
+	{
+		STANDING,
+		JUMPING,
+		DUCKING,
+		DIVING,
+	};
+
+	class Heroine
+	{
+		void handleInput(Input input);
+		void update();
+		void superBomb() {}
+	private:
+		static const int MAX_CHARGE{ 5 };
+		State state_;
+		int chargeTime_{};
+	};
+}
+
 namespace StatePattern
 {
 	enum Input
@@ -28,4 +57,11 @@ namespace StatePattern
 		int chargeTime_{};
 	};
 
+	class HerineState
+	{
+	public:
+		virtual ~HerineState() {}
+		virtual void handleInput(Heroine& heroine, Input input) {}
+		virtual void update(Heroine& heroine) {}
+	};
 }
