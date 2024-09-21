503bb10047deecff24fdddfcc4fd658f9b16c490
noodlechoi <choime321@gmail.com>
2024-09-21 15:57:40 +0900

    계층형 상태 기계

--- PracticeGame/Chapter7/State.h
+++ PracticeGame/Chapter7/State.h
@@ -183,3 +183,96 @@ namespace Double
 
 	};
 }
+
+
+namespace Step
+{
+	enum Input
+	{
+		PRESS_B,
+		PRESS_DOWN,
+		RELEASE_DOWN,
+	};
+
+	enum State
+	{
+		STANDING,
+		JUMPING,
+		DUCKING,
+		DIVING,
+	};
+
+	class Heroine
+	{
+		friend DuckingState;
+	public:
+		void handleInput(Input input)
+		{
+			HeroineState* state = state_->handleInput(*this, input);
+			if (state != nullptr) {
+				delete state_;
+				state_ = state;
+
+				state_->enter(*this);
+			}
+		}
+		void update()
+		{
+			state_->update(*this);
+		}
+		void superBomb() {}
+		static const int MAX_CHARGE{ 5 };
+	private:
+		HeroineState* state_;
+	};
+
+	class HeroineState
+	{
+	public:
+		static DuckingState ducking;
+		// 기타 상태
+		virtual ~HeroineState() {}
+		virtual void handleInput(Heroine& heroine, Input input) {}
+		virtual void update(Heroine& heroine) {}
+		virtual void enter(Heroine& heroine) {}
+	};
+
+	class OnGroundState : public HeroineState
+	{
+	public:
+		virtual void handleInput(Heroine& heroine, Input input) override
+		{
+			if(input == PRESS_B) {}
+			// 등등
+		}
+	};
+
+	class DuckingState : public OnGroundState
+	{
+	private:
+		int chargeTime_;
+	public:
+		DuckingState() : chargeTime_{} {}
+
+		virtual void handleInput(Heroine& heroine, Input input) override
+		{
+			if (input == RELEASE_DOWN) {
+			}
+			else {
+				// 따로 입력을 처리하지 않고 상위 상태로 보낸다.
+				OnGroundState::handleInput(heroine, input);
+			}
+		}
+
+		virtual void update(Heroine& heroine)
+		{
+			chargeTime_++;
+			if (chargeTime_ > Heroine::MAX_CHARGE) {
+				heroine.superBomb();
+			}
+		}
+
+		virtual void enter(Heroine& heroine) {}
+
+	};
+}
