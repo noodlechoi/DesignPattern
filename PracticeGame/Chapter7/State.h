81a61f286bfc31a6e81c835967d6ee6c20fd11b8
noodlechoi <choime321@gmail.com>
2024-09-21 15:42:32 +0900

    병행 상태 기계

--- PracticeGame/Chapter7/State.h
+++ PracticeGame/Chapter7/State.h
@@ -107,3 +107,79 @@ namespace StatePattern
 
 	};
 }
+
+
+namespace Double
+{
+	enum Input
+	{
+		PRESS_B,
+		PRESS_DOWN,
+		RELEASE_DOWN,
+	};
+
+	enum State
+	{
+		STANDING,
+		JUMPING,
+		DUCKING,
+		DIVING,
+	};
+
+	class Heroine
+	{
+		friend DuckingState;
+	public:
+		void handleInput(Input input)
+		{
+			state_->handleInput(*this, input);
+			equipment->handleInput(*this, input);
+		}
+		void update()
+		{
+			state_->update(*this);
+		}
+		void superBomb() {}
+		static const int MAX_CHARGE{ 5 };
+	private:
+		HeroineState* state_;
+		HeroineState* equipment;
+	};
+
+	class HeroineState
+	{
+	public:
+		static DuckingState ducking;
+		// 기타 상태
+		virtual ~HeroineState() {}
+		virtual HeroineState* handleInput(Heroine& heroine, Input input) {}
+		virtual void update(Heroine& heroine) {}
+		virtual void enter(Heroine& heroine) {}
+	};
+
+	class DuckingState : public HeroineState
+	{
+	private:
+		int chargeTime_;
+	public:
+		DuckingState() : chargeTime_{} {}
+
+		virtual HeroineState* handleInput(Heroine& heroine, Input input) override
+		{
+			if (input == RELEASE_DOWN) {
+				return new DuckingState();
+			}
+		}
+
+		virtual void update(Heroine& heroine)
+		{
+			chargeTime_++;
+			if (chargeTime_ > Heroine::MAX_CHARGE) {
+				heroine.superBomb();
+			}
+		}
+
+		virtual void enter(Heroine& heroine) {}
+
+	};
+}
