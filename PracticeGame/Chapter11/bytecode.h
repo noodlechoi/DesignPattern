a7216b2cfa130068550462a2512b5de7fd9b0e18
noodlechoi <choime321@gmail.com>
2024-10-05 12:59:29 +0900

    11장 가상머신 생성

--- PracticeGame/Chapter11/bytecode.h
+++ PracticeGame/Chapter11/bytecode.h
@@ -35,3 +35,55 @@ namespace EXPRESSION
 		}
 	};
 }
+
+namespace VM
+{
+	// 마법 API
+	void setHealth(int winzard, int amount) {}
+	void setWisdom(int winzard, int amount) {}
+	void setAgility(int winzard, int amount) {}
+	void playSound(int soundId) {}
+	void spawnParticles(int particleType) {}
+	static int SOUND_BANG = 0;
+	static int PARCICLE_FLAME = 0;
+
+	// 데이터 인코딩
+	enum Instruction
+	{
+		INST_SET_HEALTH = 0X00,
+		INST_SET_WISDOM = 0X01,
+		INST_SET_AGILITY = 0X02,
+		INST_SET_SOUND = 0X03,
+		INST_SET_PARICLES = 0X04,
+	};
+
+	class VM
+	{
+	public:
+		void interpret(char bytecode[], int size)
+		{
+			for (int i = 0; i < size; ++i) {
+				char instruction = bytecode[i];
+				switch (instruction) {
+				case INST_SET_HEALTH:
+					setHealth(0, 100);
+					break;
+				case INST_SET_WISDOM:
+					setWisdom(0, 100);
+					break;
+				case INST_SET_AGILITY:
+					setAgility(0, 100);
+					break;
+				case INST_SET_SOUND:
+					playSound(SOUND_BANG);
+					break;
+				case INST_SET_PARICLES:
+					spawnParticles(PARCICLE_FLAME);
+					break;
+				default:
+					break;
+				}
+			}
+		}
+	};
+}
