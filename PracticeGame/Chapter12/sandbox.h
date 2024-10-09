ec9852c5f5f78f01ae4dd66135ff15b6fe225c76
noodlechoi <choime321@gmail.com>
2024-10-09 20:25:22 +0900

    12장 두 번째 예제 코드 구현

--- PracticeGame/Chapter12/sandbox.h
+++ PracticeGame/Chapter12/sandbox.h
@@ -8,6 +8,9 @@ protected:
 	void move(double x, double y, double z) {}
 	void playSound(int sound, double volume) {}
 	void spawnParticles(int type, int count) {}
+	double getHeroX() { return 0.0f; }
+	double getHeroY() { return 0.0f; }
+	double getHeroZ() { return 0.0f; }
 };
 
 class SkyLaunch : public Superpower
@@ -15,8 +18,22 @@ class SkyLaunch : public Superpower
 protected:
 	virtual void activate() override
 	{
-		playSound(0, 1.0f);
-		spawnParticles(0, 10);
-		move(0, 0, 20);
+		if (getHeroZ() == 0) {
+			// 땅이라면 공중으로
+			playSound(0, 1.0f);
+			spawnParticles(0, 10);
+			move(0, 0, 20);
+		}
+		else if (getHeroZ() <= 10.0f) {
+			// 거의 땅이라면 이중 점프
+			playSound(0, 1.0f);
+			move(0, 0, getHeroZ() - 20);
+		}
+		else {
+			// 공중에 있으면 내려찍기
+			playSound(0, 0.7f);
+			spawnParticles(0, 1);
+			move(0, 0, -getHeroZ());
+		}
 	}
 };
