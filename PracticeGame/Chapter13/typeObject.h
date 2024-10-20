f22ddc31af352e673d84f04e2ba74cbe2b2292bf
noodlechoi <choime321@gmail.com>
2024-10-20 19:00:23 +0900

    13장 예제 코드 1

--- PracticeGame/Chapter13/typeObject.h
+++ PracticeGame/Chapter13/typeObject.h
@@ -21,3 +21,28 @@ namespace classicalOOP
 		virtual const char* getAttack() override {}
 	};
 }
+
+namespace TypeObject
+{
+	class Breed
+	{
+	private:
+		int health_;
+		const char* attack_;
+	public:
+		Breed(int health, const char* attack) : health_{health}, attack_{attack} {}
+		int getHealth() { return health_; }
+		const char* getAttack() { return attack_; }
+	};
+
+	class Monster
+	{
+	private:
+		int health_;
+		Breed& breed_;
+	public:
+		Monster(Breed& breed) : health_(breed.getHealth()), breed_{breed} {}
+		const char* getAttack() { return breed_.getAttack(); }
+	};
+
+}
