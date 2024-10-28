d489ef3c707180630df366a8a7abdc23d1b9eec9
noodlechoi <choime321@gmail.com>
2024-10-28 15:05:34 +0900

    14장 컴포넌트 예제 코드 - 분야별로 나누기

--- PracticeGame/Chapter14/component.h
+++ PracticeGame/Chapter14/component.h
@@ -1,32 +1,32 @@
 #pragma once
 
+class Volume
+{};
+class Sprite
+{};
+class World
+{
+public:
+	void resolveCollision(Volume, int, int, int) {}
+};
+class Graphics
+{
+public:
+	void draw(Sprite&, int, int) {}
+};
+class Controller
+{
+public:
+	static int getJoystickDirection() {}
+};
+enum DIR
+{
+	LEFT,
+	RIGHT,
+};
+
 namespace Tongjja
 {
-	class Volume
-	{};
-	class Sprite
-	{};
-	class World
-	{
-	public:
-		void resolveCollision(Volume, int, int, int) {}
-	};
-	class Graphics
-	{
-	public:
-		void draw(Sprite&, int, int) {}
-	};
-	class Controller
-	{
-	public:
-		static int getJoystickDirection() {}
-	};
-	enum DIR
-	{
-		LEFT,
-		RIGHT,
-	};
-
 	class Bjorn
 	{
 	private:
@@ -66,6 +66,77 @@ namespace Tongjja
 			else if (velocity_ > 0) { sprite = &spriteWaltRight_; }
 
 			graphics.draw(*sprite, x_, y_);
+		}
+	};
+}
+
+namespace Component
+{
+	class Bjorn
+	{
+	private:
+		inputComponent input_;
+		PhysicsComponent phsics_;
+		GraphicsComponent graphics_;
+
+	public:
+		int velocity_;
+		int x_, y_;
+
+		Bjorn() : velocity_{}, x_{}, y_{} {}
+		void update(World& world, Graphics& graphics)
+		{
+			input_.update(*this);
+			phsics_.update(*this, world);
+			graphics_.update(*this, graphics);
+		}
+	};
+
+	class inputComponent
+	{
+	private:
+		static const int WALK_ACCELERATION = 1;
+	public:
+		void update(Bjorn& bjorn)
+		{
+			switch (Controller::getJoystickDirection()) {
+			case LEFT:
+				bjorn.velocity_ -= WALK_ACCELERATION;
+				break;
+			case RIGHT:
+				bjorn.velocity_ += WALK_ACCELERATION;
+				break;
+			default:
+				break;
+			}
+		}
+	};
+
+	class PhysicsComponent
+	{
+	private:
+		Volume volume_;
+	public:
+		void update(Bjorn& bjorn, World& world)
+		{
+			bjorn.x_ += bjorn.velocity_;
+			world.resolveCollision(volume_, bjorn.x_, bjorn.y_, bjorn.velocity_);
+		}
+	};
+
+	class GraphicsComponent
+	{
+	private:
+		Sprite spriteStand_;
+		Sprite spriteWalkLeft_;
+		Sprite spriteWaltRight_;
+	public:
+		void update(Bjorn& bjorn, Graphics& graphics)
+		{
+			Sprite* sprite = &spriteStand_;
+			if (bjorn.velocity_ < 0) { sprite = &spriteWalkLeft_; }
+			else if (bjorn.velocity_ > 0) { sprite = &spriteWaltRight_; }
+			graphics.draw(*sprite, bjorn.x_, bjorn.y_);
 		}
 	};
 }
